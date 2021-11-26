e3c95d7adb523f47e967c26ff377e6107f0e25f0
chengkaizone <chengkaizone@163.com>
2021-11-26 20:19:32 +0800

    support androidX

--- app/src/main/java/com/lineying/androidci/MainActivity.java
+++ app/src/main/java/com/lineying/androidci/MainActivity.java
@@ -1,7 +1,8 @@
 package com.lineying.androidci;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
