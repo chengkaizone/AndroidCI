bda2d22697c306a600d0a0cf650242276fd170fd
ganchengkai <1094226429@qq.com>
2021-12-03 18:37:20 +0800

    修改update/apk为upload/apk

--- signed-apk-handle.sh
+++ signed-apk-handle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir upload && mkdir upload/apk
-mv app/build/outputs/apk/google/release/*.apk update/apk
+mv app/build/outputs/apk/google/release/*.apk upload/apk
 
 echo 'moved to update/apk'
